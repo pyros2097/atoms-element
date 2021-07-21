7df8e293f5f24519482fbb73b64e54116ab6128a
Peter John <peter@pyros.sh>
2021-07-21 21:09:40 +0530

    Update typings

--- element.d.ts
+++ element.d.ts
@@ -17,31 +17,25 @@ export declare type Location = {
   toString: () => string;
 }
 
-export default class AtomsElement {
+export class AtomsElement {
   static register(): () => void;
   static observedAttributes: Array<string>;
   static getElement: (name: string) => AtomsElement | undefined;
-  config: Config;
-  location: Location;
   attrs: {[key: string]: any};
   state: {[key: string]: any};
   computed: {[key: string]: any};
-  styles: () => string;
+  styles: {[key: string]: any};
+}
+export const getConfig = () => Config;
+export const getLocation = () => Location;
+
+export type CreateElementProps = {
+  name: () => string;
+  attrTypes: () => {[key: string]: any};
+  stateTypes: () => {[key: string]: any};
+  computedTypes: () => {[key: string]: any};
+  styles: {[key: string]: any};
+  render:  () => any
 }
 
-// declare type Colors = 'red' | 'purple' | 'blue' | 'green';
-// declare type Luminance = 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
-// declare type BgColor = `bg-${Colors}-${Luminance}`;
-// declare type Distance = 0.5 | 1 | 1.5 | 2 | 2.5 | 3 | 3.5 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
-// declare type Breakpoints = 'xs:' | 'sm:' | 'md:' | 'lg:' | 'xl:' | '';
-// declare type Space = `${Breakpoints}space-${'x' | 'y'}-${Distance}`;
-// declare type ValidClass = Space | BgColor;
-// declare type Tailwind<S> = S extends `${infer Class} ${infer Rest}` ? Class extends ValidClass ? `${Class} ${Tailwind<Rest>}` : never : S extends `${infer Class}` ? Class extends ValidClass ? S : never : never;
-// declare function doSomethingWithTwClass<S>(cls: Tailwind<S>): Tailwind<S>;
-// declare const bad: never;
-// declare const bad2: never;
-// declare const bad3: never;
-// declare const bad4: never;
-// declare const good: "bg-red-400 space-x-4 md:space-x-8";
-// declare const good2: "bg-red-400 space-x-4";
-// declare const good3: "space-x-1.5 bg-blue-200";
+export const createElement = (props: CreateElementProps) => CreateElementProps;
