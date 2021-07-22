7dc696c6831d3cd4ce054112d35f76c328c4b446
Peter John <peter@pyros.sh>
2021-07-22 23:36:07 +0530

    implement easy html parsing

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,17 @@
-import { Config } from './page';
+export declare type Config = {
+  version: string
+  url: string
+  image: string
+  author: string
+  languages: Array<string>
+  title: string
+  description: string
+  keywords: string
+  categories: Array<any>
+  tags: Array<any>
+  strings: {[key: string]: any}
+  themes: {[key: string]: any}
+}
 
 export declare type Location = {
   readonly ancestorOrigins: DOMStringList;
@@ -16,6 +29,9 @@ export declare type Location = {
   replace: (url: string | URL) => void;
   toString: () => string;
 }
+
+export declare type Data = any;
+export declare type Item = any;
 
 export class AtomsElement {
   static register(): () => void;
@@ -37,3 +53,27 @@ export type CreateElementProps = {
 }
 
 export const createElement = (props: CreateElementProps) => CreateElementProps;
+
+export type HandlerProps = {
+  config: Config;
+  data: Data;
+  item: Item;
+}
+export type Handler = (props: HandlerProps) => string;
+
+export type CreatePageProps = {
+  route: Handler;
+  datapaths: Handler;
+  head: Handler;
+  body: Handler;
+}
+
+export type PageRenderProps = {
+  config: Config;
+  data: Data;
+  item: Item; 
+  headScript: string; 
+  bodyScript: string;
+}
+
+export const createPage = (props: CreatePageProps) => (props: PageRenderProps) => string;
--- index.js
+++ index.js
@@ -375,7 +375,7 @@ const classLookup = {
 export const getTWStyleSheet = (template) => {
   const classList = template.strings
     .reduce((acc, item) => {
-      const matches = item.match(/(?:class|className)=(?:["']\W+\s*(?:\w+)\()?["']([^'"]+)['"]/gim);
+      const matches = item.match(/class=(?:["']\W+\s*(?:\w+)\()?["']([^'"]+)['"]/gim);
       if (matches) {
         matches.forEach((matched) => {
           acc += matched.replace('class="', '').replace('"', '') + ' ';
@@ -415,6 +415,239 @@ const wrapAttribute = (attrName, suffix, text, v) => {
     buffer += `"`;
   }
   return buffer;
+};
+
+const tagRE = /<[a-zA-Z0-9\-\!\/](?:"[^"]*"|'[^']*'|[^'">])*>/g;
+const whitespaceRE = /^\s*$/;
+const attrRE = /\s([^'"/\s><]+?)[\s/>]|([^\s=]+)=\s?(".*?"|'.*?')/g;
+const voidElements = {
+  area: true,
+  base: true,
+  br: true,
+  col: true,
+  embed: true,
+  hr: true,
+  img: true,
+  input: true,
+  link: true,
+  meta: true,
+  param: true,
+  source: true,
+  track: true,
+  wbr: true,
+};
+const empty = Object.create(null);
+
+const parseTag = (tag) => {
+  const res = {
+    type: 'tag',
+    name: '',
+    voidElement: false,
+    attrs: {},
+    children: [],
+  };
+
+  const tagMatch = tag.match(/<\/?([^\s]+?)[/\s>]/);
+  if (tagMatch) {
+    res.name = tagMatch[1];
+    if (voidElements[tagMatch[1]] || tag.charAt(tag.length - 2) === '/') {
+      res.voidElement = true;
+    }
+
+    // handle comment tag
+    if (res.name.startsWith('!--')) {
+      const endIndex = tag.indexOf('-->');
+      return {
+        type: 'comment',
+        comment: endIndex !== -1 ? tag.slice(4, endIndex) : '',
+      };
+    }
+  }
+
+  const reg = new RegExp(attrRE);
+  let result = null;
+  for (;;) {
+    result = reg.exec(tag);
+
+    if (result === null) {
+      break;
+    }
+
+    if (!result[0].trim()) {
+      continue;
+    }
+
+    if (result[1]) {
+      const attr = result[1].trim();
+      let arr = [attr, ''];
+
+      if (attr.indexOf('=') > -1) {
+        arr = attr.split('=');
+      }
+
+      res.attrs[arr[0]] = arr[1];
+      reg.lastIndex--;
+    } else if (result[2]) {
+      res.attrs[result[2]] = result[3].trim().substring(1, result[3].length - 1);
+    }
+  }
+
+  return res;
+};
+const parseHtml = (html, options) => {
+  options || (options = {});
+  options.components || (options.components = empty);
+  const result = [];
+  const arr = [];
+  let current;
+  let level = -1;
+  let inComponent = false;
+
+  // handle text at top level
+  if (html.indexOf('<') !== 0) {
+    var end = html.indexOf('<');
+    result.push({
+      type: 'text',
+      content: end === -1 ? html : html.substring(0, end),
+    });
+  }
+
+  html.replace(tagRE, function (tag, index) {
+    if (inComponent) {
+      if (tag !== '</' + current.name + '>') {
+        return;
+      } else {
+        inComponent = false;
+      }
+    }
+    const isOpen = tag.charAt(1) !== '/';
+    const isComment = tag.startsWith('<!--');
+    const start = index + tag.length;
+    const nextChar = html.charAt(start);
+    let parent;
+
+    if (isComment) {
+      const comment = parseTag(tag);
+
+      // if we're at root, push new base node
+      if (level < 0) {
+        result.push(comment);
+        return result;
+      }
+      parent = arr[level];
+      parent.children.push(comment);
+      return result;
+    }
+
+    if (isOpen) {
+      level++;
+
+      current = parseTag(tag);
+      if (current.type === 'tag' && options.components[current.name]) {
+        current.type = 'component';
+        inComponent = true;
+      }
+
+      if (!current.voidElement && !inComponent && nextChar && nextChar !== '<') {
+        current.children.push({
+          type: 'text',
+          content: html.slice(start, html.indexOf('<', start)),
+        });
+      }
+
+      // if we're at root, push new base node
+      if (level === 0) {
+        result.push(current);
+      }
+
+      parent = arr[level - 1];
+
+      if (parent) {
+        parent.children.push(current);
+      }
+
+      arr[level] = current;
+    }
+
+    if (!isOpen || current.voidElement) {
+      if (level > -1 && (current.voidElement || current.name === tag.slice(2, -1))) {
+        level--;
+        // move current up a level to match the end tag
+        current = level === -1 ? result : arr[level];
+      }
+      if (!inComponent && nextChar !== '<' && nextChar) {
+        // trailing text node
+        // if we're at the root, push a base text node. otherwise add as
+        // a child to the current node.
+        parent = level === -1 ? result : arr[level].children;
+
+        // calculate correct end of the content slice in case there's
+        // no tag after the text node.
+        const end = html.indexOf('<', start);
+        let content = html.slice(start, end === -1 ? undefined : end);
+        // if a node is nothing but whitespace, collapse it as the spec states:
+        // https://www.w3.org/TR/html4/struct/text.html#h-9.1
+        if (whitespaceRE.test(content)) {
+          content = ' ';
+        }
+        // don't add whitespace-only text nodes if they would be trailing text nodes
+        // or if they would be leading whitespace-only text nodes:
+        //  * end > -1 indicates this is not a trailing text node
+        //  * leading node is when level is -1 and parent has length 0
+        if ((end > -1 && level + parent.length >= 0) || content !== ' ') {
+          parent.push({
+            type: 'text',
+            content: content,
+          });
+        }
+      }
+    }
+  });
+
+  return result;
+};
+
+const stringifyAttrs = (attrs) => {
+  const buff = [];
+  for (let key in attrs) {
+    buff.push(key + '="' + attrs[key] + '"');
+  }
+  if (!buff.length) {
+    return '';
+  }
+  return ' ' + buff.join(' ');
+};
+
+const stringifyHtml = (buff, doc) => {
+  switch (doc.type) {
+    case 'text':
+      return buff + doc.content;
+    case 'tag':
+      buff += '<' + doc.name + (doc.attrs ? stringifyAttrs(doc.attrs) : '') + (doc.voidElement ? '/>' : '>');
+      if (doc.voidElement) {
+        return buff;
+      }
+      return buff + doc.children.reduce(stringifyHtml, '') + '</' + doc.name + '>';
+    case 'comment':
+      buff += '<!--' + doc.comment + '-->';
+      return buff;
+  }
+};
+
+const hydrate = (node) => {
+  if (node.children) {
+    for (const child of node.children) {
+      const Clazz = AtomsElement.getElement(child.name);
+      if (Clazz) {
+        const instance = new Clazz(child.attrs);
+        const res = instance.renderTemplate();
+        child.children.push(...parseHtml(res));
+      }
+      if (child.children) {
+        hydrate(child);
+      }
+    }
+  }
 };
 
 export const render = isBrowser
@@ -460,7 +693,14 @@ export const render = isBrowser
           // console.log('value', value);
         }
       });
-      return js;
+      const nodes = parseHtml(js);
+      for (const node of nodes) {
+        hydrate(node);
+      }
+      const html = nodes.reduce((acc, node) => {
+        return acc + stringifyHtml('', node);
+      }, '');
+      return html;
     };
 
 const previousValues = new WeakMap();
@@ -705,7 +945,7 @@ export class AtomsElement extends BaseElement {
   get attrs() {
     return Object.keys(this.constructor.attrTypes).reduceRight((acc, key) => {
       const attrType = this.constructor.attrTypes[key];
-      const newValue = isBrowser ? this.getAttribute(key.toLowerCase()) : this.ssrAttributes.find((item) => item.name === key.toLowerCase())?.value;
+      const newValue = isBrowser ? this.getAttribute(key.toLowerCase()) : this.ssrAttributes[key.toLowerCase()];
       const data = attrType.parse(newValue);
       attrType.validate(`<${this.constructor.name}> ${key}`, data);
       acc[key] = data;
@@ -790,4 +1030,42 @@ export const createElement = ({ name, attrTypes, stateTypes, computedTypes, rend
   };
   Element.register();
   return { name, attrTypes, stateTypes, computedTypes, render };
+};
+
+export const createPage = ({ route, datapaths, head, body }) => {
+  return ({ config, data, item, headScript, bodyScript }) => {
+    const isProd = process.env.NODE_ENV === 'production';
+    const props = { config, data, item };
+    const headHtml = render(head(props));
+    const bodyTemplate = body(props);
+    const bodyHtml = render(bodyTemplate);
+    return `
+      <!DOCTYPE html>
+      <html lang="${config.lang}">
+        <head>
+          <meta charset="utf-8" />
+          <meta http-equiv="x-ua-compatible" content="ie=edge" />
+          <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=5.0, shrink-to-fit=no">
+          <link rel="sitemap" type="application/xml" href="/sitemap.xml" />
+          <link rel="icon" type="image/png" href="/assets/icon.png" />
+          ${headHtml}
+          <style>
+            ${getTWStyleSheet(bodyTemplate)}
+          </style>
+          ${headScript}
+        </head>
+        <body>
+          ${bodyHtml}
+          <script>
+            window.__DEV__ = ${!isProd};
+            window.config = ${JSON.stringify(config)};
+            window.data = ${JSON.stringify(data)};
+            window.item = ${JSON.stringify(item)};
+          </script>
+          ${bodyScript}
+        </body>
+      </html>
+  `;
+  };
 };
